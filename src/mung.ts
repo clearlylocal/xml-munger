67a5ce2f659e5204680b8208886576f4f9916fc9
lionel-rowe <lionel.rowe@gmail.com>
2024-03-07 16:33:47 +0800

    Initial commit

---
+++ src/mung.ts
@@ -0,0 +1,155 @@
+// deno-lint-ignore-file no-fallthrough
+import type { Cheerio, Element } from 'cheerio'
+import { load } from 'cheerio'
+import { escape, unescape } from 'std/html/entities.ts'
+import { createParser, render } from 'css-selector-parser'
+import { assert } from 'std/assert/assert.ts'
+import { unimplemented } from 'std/assert/unimplemented.ts'
+import { unreachable } from 'std/assert/unreachable.ts'
+
+const parseSelector = createParser()
+
+export type MungParams = {
+	/** Apply this function to the content before writing it */
+	transform: (x: string) => string
+	/** Run for each matching element of selector */
+	each: string
+	/** Get the content of this selector */
+	from: string
+	/** Write the content to this selector */
+	to: string
+	/**
+	 * Overwrite existing content if `to` selector is populated
+	 * @default false
+	 */
+	overwrite?: boolean
+	/**
+	 * Skip content matching this regular expression or matcher (write matched content as-is)
+	 */
+	skip?: Pick<RegExp, typeof Symbol.matchAll>
+}
+
+function skipBy(input: string, skip: Pick<RegExp, typeof Symbol.matchAll>) {
+	const out: string[] = []
+
+	let idx = 0
+	for (const m of skip[Symbol.matchAll](input)) {
+		out.push(input.slice(idx, m.index))
+		out.push(m[0])
+		idx = m.index! + m[0].length
+	}
+
+	out.push(input.slice(idx))
+
+	return out
+}
+
+export function mung(xml: string, { transform, each, from, to, overwrite, skip }: MungParams) {
+	const $ = load(xml, { xml: true })
+
+	const rootToken = parseSelector(to)
+	assert(rootToken.rules.length === 1)
+
+	for (const el of $(each)) {
+		const $el = $(el) as Cheerio<Element>
+		const $from = $el.find(`:scope > ${from}`).eq(0)
+		let $to = $el.find(`:scope > ${to}`).eq(0)
+
+		if (!$to.length) {
+			let $cur = $el
+
+			let rule = structuredClone(rootToken.rules[0])
+
+			while (true) {
+				assert(rule.items.length)
+				assert(rule.combinator == null || rule.combinator === '>')
+				assert(rule.items[0].type === 'TagName')
+
+				const subSelector = `:scope ${render({ ...rule, combinator: '>', nestedRule: undefined })}`
+				$to = $cur.find(subSelector)
+
+				if ($to.length) {
+					if (rule.nestedRule) {
+						rule = rule.nestedRule
+						$cur = $to
+					} else {
+						break
+					}
+				} else {
+					const tag = rule.items[0].name
+					const attrs: Record<string, string> = {}
+					for (const item of rule.items.slice(1)) {
+						switch (item.type) {
+							case 'ClassName': {
+								attrs.class = item.name
+								break
+							}
+							case 'Attribute': {
+								if (!item.value) {
+									attrs[item.name] = ''
+									break
+								}
+								assert(item.value.type === 'String')
+								attrs[item.name] = item.value.value
+								break
+							}
+							// `Id` unsupported as IDs must be unique
+							case 'Id':
+							// some kinds of `PseudoClass`, e.g. `:not`, might possibly be supported in future
+							case 'PseudoClass':
+							// `TagName` can only be first item
+							case 'TagName':
+							// `WildcardTag` and `PseudoElement` unclear what they would mean in this context
+							case 'WildcardTag':
+							case 'PseudoElement': {
+								unimplemented(`Unsupported selector item type: ${item.type}`)
+							}
+							default: {
+								unreachable()
+							}
+						}
+					}
+
+					$to = $(`<${tag}>` as 'xml-element')
+					for (const [k, v] of Object.entries(attrs)) {
+						$to.attr(k, v)
+					}
+					$cur.append($to)
+
+					if (rule.nestedRule) {
+						rule = rule.nestedRule
+						$cur = $to
+					} else {
+						break
+					}
+				}
+			}
+		}
+
+		if (!overwrite && $to.html()) {
+			continue
+		}
+
+		$to.html(
+			($from.html() ?? '')
+				.split(/(<[^>]+>)/)
+				.flatMap((x, i) => {
+					if (i % 2) {
+						// is start/end tag
+						return x
+					}
+
+					const unescaped = unescape(x)
+					const segs = skip ? skipBy(unescaped, skip) : [unescaped]
+
+					return segs.map((x, i) => escape(i % 2 ? x : transform(x)))
+				})
+				.join(''),
+		)
+	}
+
+	const x = $.xml()
+
+	// we unescape then re-escape entities to ensure nothing is escaped unnecessarily
+	return x.replaceAll(/&#?\w+;/gu, (m) => escape(unescape(m)))
+}
---
+++ tests/mung.test.ts
@@ -0,0 +1,229 @@
+import { load } from 'cheerio'
+import { mung, type MungParams } from '../src/mung.ts'
+import { assertEquals } from 'std/assert/mod.ts'
+import { join } from 'std/path/mod.ts'
+
+const fixturesDirPath = './tests/fixtures/'
+const xliffFileName = 'example.xliff'
+
+Deno.test(mung.name, async (t) => {
+	await t.step('Simple example', () => {
+		const xml = `<xml>
+			<g>
+				<from>Sender 1</from>
+				<to/>
+			</g>
+			<g>
+				<from>Sender 2</from>
+				<to>Recipient 2</to>
+			</g>
+		</xml>`
+
+		const out = mung(xml, {
+			each: 'g',
+			from: 'from',
+			to: 'to',
+			transform: (s) => s.toUpperCase(),
+			overwrite: true,
+		})
+
+		assertEquals(
+			out,
+			`<xml>
+			<g>
+				<from>Sender 1</from>
+				<to>SENDER 1</to>
+			</g>
+			<g>
+				<from>Sender 2</from>
+				<to>SENDER 2</to>
+			</g>
+		</xml>`,
+		)
+	})
+
+	await t.step(xliffFileName, async (t) => {
+		const xliff = await Deno.readTextFile(join(fixturesDirPath, xliffFileName))
+
+		const xliffTestDefaults = {
+			each: 'trans-unit',
+			from: 'source',
+			to: 'target',
+			transform: (s) => s.toUpperCase(),
+		} satisfies Partial<MungParams>
+
+		await t.step('Content', async (t) => {
+			const defaults = xliffTestDefaults
+
+			await t.step('basic', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#basic > target').text(), 'BASIC')
+			})
+
+			await t.step('skips populated target (default)', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#populated-target > target').text(), '...')
+			})
+
+			await t.step('overwrites populated target (if configured)', () => {
+				const out = mung(xliff, {
+					...defaults,
+					overwrite: true,
+				})
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#populated-target > target').text(), 'POPULATED TARGET')
+			})
+
+			await t.step('skips start and end tags (but not contents)', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#contains-tag > target').html(), 'CONTAINS <g>TAG</g> + TRAILING')
+			})
+
+			await t.step('skips placeholder by regex', () => {
+				const out = mung(xliff, {
+					...defaults,
+					skip: /\{[\w]+\}/gu,
+				})
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#contains-placeholder > target').text(), 'CONTAINS {placeholder} + TRAILING')
+			})
+
+			await t.step('skips angle-bracket placeholder by unescaped regex', () => {
+				const out = mung(xliff, {
+					...defaults,
+					skip: /<[\w]+>/gu,
+				})
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#contains-angle-placeholder > target')
+				assertEquals($el.text(), 'CONTAINS <placeholder> + TRAILING')
+				assertEquals($el.html(), 'CONTAINS &lt;placeholder&gt; + TRAILING')
+			})
+
+			await t.step('skips URL by regex', () => {
+				const out = mung(xliff, {
+					...defaults,
+					skip: /https?:\/\/\S+/gu,
+				})
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#contains-url > target').text(), 'CONTAINS https://example.com/url')
+			})
+		})
+
+		await t.step('<alt-trans origin="mt">', async (t) => {
+			const defaults = {
+				...xliffTestDefaults,
+				to: 'alt-trans[origin="mt"] > target',
+			}
+
+			await t.step('no-alt-trans', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#no-alt-trans > alt-trans[origin="mt"] > target')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), 'NO ALT-TRANS')
+			})
+
+			await t.step('no-alt-trans (adds attributes)', () => {
+				const out = mung(xliff, {
+					...defaults,
+					to: 'alt-trans[origin="mt"] > target.class[attr][name=val]',
+				})
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#no-alt-trans > alt-trans[origin="mt"] > target.class[attr][name=val]')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), 'NO ALT-TRANS')
+			})
+
+			await t.step('empty-alt-trans-mt', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#empty-alt-trans-mt > alt-trans[origin="mt"] > target')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), 'EMPTY ALT-TRANS')
+			})
+
+			await t.step('empty-alt-trans-mt-target', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#empty-alt-trans-mt-target > alt-trans[origin="mt"] > target')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), 'EMPTY ALT-TRANS TARGET')
+			})
+
+			await t.step('populated-alt-trans-mt skips populated target (default)', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#populated-alt-trans-mt > alt-trans[origin="mt"] > target')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), '...')
+			})
+
+			await t.step('populated-alt-trans-mt overwrites populated target (if configured)', () => {
+				const out = mung(xliff, {
+					...defaults,
+					to: 'alt-trans[origin="mt"] > target',
+					overwrite: true,
+				})
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#populated-alt-trans-mt > alt-trans[origin="mt"] > target')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), 'POPULATED ALT-TRANS')
+			})
+
+			await t.step('alt-trans-other', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#alt-trans-other > alt-trans[origin="mt"]').length, 1)
+				assertEquals($('trans-unit#alt-trans-other > alt-trans[origin="mt"]').text(), 'ALT-TRANS OTHER')
+
+				assertEquals($('trans-unit#alt-trans-other > alt-trans[origin="other"]').length, 1)
+				assertEquals($('trans-unit#alt-trans-other > alt-trans[origin="other"]').text(), '...')
+			})
+
+			await t.step('alt-trans-target-before-main-target', () => {
+				const out = mung(xliff, defaults)
+				const $ = load(out, { xml: true })
+
+				const $el = $('trans-unit#alt-trans-target-before-main-target > alt-trans[origin="mt"] > target')
+				assertEquals($el.length, 1)
+				assertEquals($el.text(), 'ALT-TRANS TARGET BEFORE MAIN TARGET')
+
+				assertEquals($('trans-unit#alt-trans-target-before-main-target > target').text(), '...')
+			})
+		})
+
+		await t.step('Overwrite self', async (t) => {
+			const defaults = xliffTestDefaults
+
+			await t.step('basic', () => {
+				const out = mung(xliff, {
+					...defaults,
+					to: 'source',
+					overwrite: true,
+				})
+
+				const $ = load(out, { xml: true })
+
+				assertEquals($('trans-unit#basic > target').text(), '')
+				assertEquals($('trans-unit#basic > source').text(), 'BASIC')
+			})
+		})
+	})
+})
